6629134d80acaac72bbde4758e3503ab480fb23e
Dmitry Grigoryev <dima@highweb.ru>
2010-09-27 12:49:11 +0400

    Interpreter now works correctly with several cells in parameter thread

--- tutforth.asm
+++ tutforth.asm
@@ -26,6 +26,7 @@ section .text
 
 next:
         mov ebx, [eax]          ; W now points to code field of next word
+        add eax, 4              ; Adjust IP to next word in thread
         mov edx, [ebx]          ; X now points to machine code of next word
         jmp edx                 ; Jump to word machine code
 
@@ -39,28 +40,31 @@ enter:
 exit:
 
         align   4
-test_code_xt:
-        dd      test_code_xt+4  ; address of first instruction
+print_xt:
+        dd      print_xt+4      ; Code field
+        mov esi, eax            ; Save IP
         mov eax, 4              ; sys_write
         mov ebx, 1              ; Standard output
         mov ecx, test_str       ; Address of string
         mov edx, test_str_len   ; Length of string
         int 80h                 ; Make syscall
+        mov eax, esi            ; Restore IP
+        jmp next                ; End of code
 
+        align   4
+sys_exit_xt:
+        dd      sys_exit_xt+4   ; Code field
         mov eax, 1              ; sys_exit
         mov ebx, 0              ; Return code 0 - success
         int 80h                 ; Make syscall
 
-        jmp next                ; End of code
-
         align   4
-test_itc_xt:
+main_xt:
         dd      enter           ; Code field
-        dd      test_code_xt
-        dd      exit
+        dd      print_xt, sys_exit_xt
 
 start_ip:
-        dd      test_itc_xt
+        dd      main_xt
 
 _start:
         nop                     ; Makes gdb happy
