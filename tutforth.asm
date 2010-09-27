05b8bab57d9a3805a44848f34d8821c376255435
Dmitry Grigoryev <dima@highweb.ru>
2010-09-27 23:02:40 +0400

    Renamed @ (get), implemeted ! (put), + (add), literals (lit)

--- tutforth.asm
+++ tutforth.asm
@@ -4,6 +4,7 @@ section .data
         hello_str_ptr   dd      hello_str
 section .bss
         alignb  4
+dictionary:
         resd    64
 pstack_bottom:
         resd    32
@@ -23,7 +24,16 @@ section .text
 ; EDI: -
 ; EBP: (RSP) return stack pointer
 ; ESP: (PSP) parameter stack pointer
-; 
+
+_start:
+        nop                     ; Makes gdb happy
+
+        mov eax, start_ip       ; Initialize IP
+        mov ebp, rstack_bottom  ; Initialize RSP
+        mov esp, pstack_bottom  ; Initialize PSP
+        mov edx, 0              ; Initialize TOS
+
+        jmp next                ; Jump to interpreter
 
 next:
         mov ebx, [eax]          ; W now points to code field of next word
@@ -52,24 +62,54 @@ swap:                           ; x1 x2 -- x2 x1
         jmp next
 
         align   4
-deref:                          ; addr -- x
-        dd      deref+4         ; Code field
-        mov edx, [edx]
-        jmp next
+get:                            ; addr -- x
+        dd      get+4           ; Code field
+        mov edx, [edx]          ; Get X from ADDR
+        jmp next                ; Jump to interpreter
 
         align   4
-hello:
+put:                            ; x addr --
+        dd      put+4           ; Code field
+        pop ebx                 ; Get X value
+        mov [edx], ebx          ; Store X at ADDR
+        pop edx                 ; Get new TOS
+        jmp next                ; Jump to interpreter
+
+        align   4
+lit:                            ; -- x
+        dd      lit+4
+        push edx                ; Store old TOS
+        mov edx, [eax]          ; Get X from thread
+        add eax, 4              ; Adjust IP
+        jmp next                ; Jump to interpreter
+
+        align   4
+add:                            ; n1 n2 -- n
+        dd      add+4
+        pop ebx                 ; Get N1 from the stack
+        add edx, ebx            ; Add N1 to TOS=N2
+        jmp next                ; Jump to interpreter
+
+        align   4
+hello:                          ; -- addr u
         dd      hello+4
         push edx
-        push hello_str_ptr
+        push hello_str
         mov edx, hello_str_len
         jmp next
 
         align   4
-print:
+dict:                           ; -- addr
+        dd      dict+4
+        push edx                ; Store old TOS
+        mov edx, dictionary     ; Get ADDR of dictionary
+        jmp next                ; Jump to interpreter
+
+        align   4
+print:                          ; addr u --
         dd      print+4         ; Code field
         mov esi, eax            ; Save IP
-        mov edi, edx            ; String length is already in edx
+                                ; String length is already in edx
         mov eax, 4              ; sys_write
         mov ebx, 1              ; Standard output
         pop ecx                 ; Address of string
@@ -79,27 +119,23 @@ print:
         jmp next                ; End of code
 
         align   4
-sys_exit:
+sys_exit:                       ; --
         dd      sys_exit+4      ; Code field
         mov eax, 1              ; sys_exit
         mov ebx, 0              ; Return code 0 - success
         int 80h                 ; Make syscall
 
         align   4
-get_str:
-        dd      enter, hello, swap, deref, swap, exit
+cell1:                          ; -- addr
+        dd      enter, dict, exit
+cell2:                          ; -- addr
+        dd      enter, dict, lit, 4, add, exit
+get_str:                        ; -- addr u
+        dd      enter, cell2, get, cell1, get, exit
+init:                           ; --
+        dd      enter, hello, cell1, put, cell2, put, exit
 main:
-        dd      enter, get_str, get_str, print, print, sys_exit
+        dd      enter, init, get_str, get_str, print, print, sys_exit
 start_ip:
         dd      main
-
-_start:
-        nop                     ; Makes gdb happy
-
-        mov eax, start_ip       ; Initialize IP
-        mov ebp, rstack_bottom  ; Initialize RSP
-        mov esp, pstack_bottom  ; Initialize PSP
-        mov edx, 0              ; Initialize TOS
-
-        jmp next                ; Jump to interpreter
 
